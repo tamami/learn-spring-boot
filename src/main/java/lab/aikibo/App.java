3b4227984aa280a849a21daad4f32cdd4a2115a8
tamami <tamami.oka@gmail.com>
2016-11-19 22:47:04 +0700

    update content

--- src/main/java/lab/aikibo/App.java
+++ src/main/java/lab/aikibo/App.java
@@ -1,13 +1,26 @@
 package lab.aikibo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	ApplicationContext ctx = SpringApplication.run(App.class, args);
+
+	System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+	String[] beanNames = ctx.getBeanDefinitionNames();
+	Arrays.sort(beanNames);
+	for(String beanName : beanNames) {
+	  System.out.println(beanName);
+	}
     }
 }
